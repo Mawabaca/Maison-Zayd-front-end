d522d33b508326a8550f1de93e20c615ff6964a0
Abderahmane <abderahmane.benomari7@gmail.com>
2024-05-26 22:34:07 +0200

    kk
    kkk

--- components/FlyingButton.js
+++ components/FlyingButton.js
@@ -1,38 +1,43 @@
+import { useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import {ButtonStyle} from "@/components/Button";
-import {primary} from "@/lib/colors";
-import {CartContext} from "@/components/CartContext";
-import {useContext, useEffect, useRef, useState} from "react";
+import { ButtonStyle } from "@/components/Button";
+import { primary } from "@/lib/colors";
+import { CartContext } from "@/components/CartContext";
 
 const FlyingButtonWrapper = styled.div`
-  button{
+  button {
     ${ButtonStyle};
-    ${props => props.main ? `
+    ${(props) =>
+      props.main
+        ? `
       background-color: ${primary};
       color:white;
-    ` : `
+    `
+        : `
       background-color: transparent;
       border: 1px solid ${primary};
       color:${primary};
     `}
-    ${props => props.white && `
+    ${(props) =>
+      props.white &&
+      `
       background-color: white;
       border: 1px solid white;
       font-weight:500;
     `}
   }
-  @keyframes fly{
-    100%{
-      top:0;
-      left:65%;
+  @keyframes fly {
+    100% {
+      top: 0;
+      left: 65%;
       opacity: 0;
-      display:none;
+      display: none;
       max-width: 50px;
       max-height: 50px;
     }
   }
-  img{
-    display:none;
+  img {
+    display: none;
     max-width: 100px;
     max-height: 100px;
     opacity: 1;
@@ -44,36 +49,45 @@ const FlyingButtonWrapper = styled.div`
 `;
 
 export default function FlyingButton(props) {
-  const {addProduct} = useContext(CartContext);
+  const { addProduct } = useContext(CartContext);
   const imgRef = useRef();
+  const timeoutRef = useRef();
+
   function sendImageToCart(ev) {
-    imgRef.current.style.display = 'inline-block';
-    imgRef.current.style.left = (ev.clientX-50) + 'px';
-    imgRef.current.style.top = (ev.clientY-50) + 'px';
-    setTimeout(() => {
-      imgRef.current.style.display = 'none';
-    }, 1000);
+    if (imgRef.current) {
+      imgRef.current.style.display = "inline-block";
+      imgRef.current.style.left = ev.clientX - 50 + "px";
+      imgRef.current.style.top = ev.clientY - 50 + "px";
+      timeoutRef.current = setTimeout(() => {
+        if (imgRef.current) {
+          imgRef.current.style.display = "none";
+        }
+      }, 1000);
+    }
   }
+
   useEffect(() => {
     const interval = setInterval(() => {
-      const reveal = imgRef.current?.closest('div[data-sr-id]');
-      if (reveal?.style.opacity === '1') {
-        // visible
-        reveal.style.transform = 'none';
+      const reveal = imgRef.current?.closest("div[data-sr-id]");
+      if (reveal?.style.opacity === "1") {
+        reveal.style.transform = "none";
       }
     }, 100);
 
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeoutRef.current);
+    };
   }, []);
+
   return (
-    <>
-      <FlyingButtonWrapper
-        white={props.white}
-        main={props.main}
-        onClick={() => addProduct(props._id)}>
-        <img src={props.src} alt="" ref={imgRef} />
-        <button onClick={ev => sendImageToCart(ev)} {...props} />
-      </FlyingButtonWrapper>
-    </>
+    <FlyingButtonWrapper
+      white={props.white}
+      main={props.main}
+      onClick={() => addProduct(props._id)}
+    >
+      <img src={props.src} alt="" ref={imgRef} />
+      <button onClick={(ev) => sendImageToCart(ev)} {...props} />
+    </FlyingButtonWrapper>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components';
+import React from 'react';
+import { FaTwitterSquare, FaFacebookSquare, FaPinterestSquare, FaInstagramSquare } from 'react-icons/fa';
+
+const FooterHeader = styled.h3`
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+const FooterLink = styled.a`
+  color: #ffffff;
+  text-decoration: none;
+  display: block;
+  margin-bottom: 8px;
+  font-size: 16px;
+`;
+
+const FooterContainer = styled.div`
+  background-color: #1c1c1c;
+  color: #ffffff;
+  padding: 40px 20px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  border-top: 2px solid #d4bbb4;
+  text-align: center;
+  margin-top: 50px;
+`;
+
+const FooterBlock = styled.div`
+  flex: 1 1 30%;
+  margin-bottom: 20px;
+`;
+
+const FooterInput = styled.input`
+  width: calc(100% - 20px);
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+const FooterButton = styled.button`
+  width: calc(100% - 20px);
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const SocialIcons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const SocialIconLink = styled.a`
+  text-decoration: none;
+`;
+
+const CopyrightText = styled.p`
+  width: 100%;
+  text-align: center;
+  color: #ffffff;
+  margin: 10px 0;
+  border-top-width: 1px;
+  border-top-color: rgba(255,255,255,0.66);
+`;
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <FooterBlock>
+        <FooterHeader>Accès rapide</FooterHeader>
+        <FooterLink href="">Notre histoire</FooterLink>
+        <FooterLink href="">Avis</FooterLink>
+        <FooterLink href="">La boutique</FooterLink>
+        {/* Ajoutez d'autres liens ici */}
+      </FooterBlock>
+      <FooterBlock>
+        <FooterHeader>Suivez-nous</FooterHeader>
+        <SocialIcons>
+          <SocialIconLink href="https://maisonsikha.fr/products/berangere" target="_blank">
+            <FaFacebookSquare size={30} />
+          </SocialIconLink>
+          <SocialIconLink href="https://maisonsikha.fr/products/berangere" target="_blank">
+            <FaTwitterSquare size={30} />
+          </SocialIconLink>
+          <SocialIconLink href="https://maisonsikha.fr/products/berangere" target="_blank">
+            <FaInstagramSquare size={30} />
+          </SocialIconLink>
+          <SocialIconLink href="https://maisonsikha.fr/products/berangere" target="_blank">
+            <FaPinterestSquare size={30} />
+          </SocialIconLink>
+        </SocialIcons>
+        <FooterInput type="text" placeholder="Nom" />
+        <FooterInput type="email" placeholder="Adresse email" />
+        <FooterButton>Je m'abonne</FooterButton>
+      </FooterBlock>
+      <FooterBlock>
+        <FooterHeader>Besoin d’aide</FooterHeader>
+        <FooterLink href="">Nous contacter</FooterLink>
+        <FooterLink href="">La livraison</FooterLink>
+        <FooterLink href="">Retour, échange et rétractation</FooterLink>
+        {/* Ajoutez d'autres liens ici */}
+      </FooterBlock>
+      <CopyrightText>Copyright © 2024 - Maison Zayd</CopyrightText>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
