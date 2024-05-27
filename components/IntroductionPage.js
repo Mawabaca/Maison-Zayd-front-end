efa5b479ceb3f34a75f85825cfadfd53cc388053
Abderahmane <abderahmane.benomari7@gmail.com>
2024-05-27 14:03:42 +0200

    ss
    ss

--- components/IntroductionPage.js
+++ components/IntroductionPage.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const IntroContainer = styled.div`
   display: flex;
@@ -7,33 +8,37 @@ const IntroContainer = styled.div`
   justify-content: center;
   width: 100vw;
   height: 100vh;
-  background-color: #b8887a;
+  background-color: #AC7C6B;
   margin: 0;
   padding: 0;
 `;
 
 const Video = styled.video`
+  margin-bottom: 150px;
   width: 80%;
-  height: auto;
+  height: 90%;
   border: none;
 `;
 
 const DiscoverButton = styled.button`
-  margin-top: 20px;
-  padding: 10px 20px;
-  background-color: #fff;
-  border: none;
+  position: absolute; 
+  bottom: 200px; 
+  right: 600px; 
+  border: 3px solid white;
+  padding: 15px 30px; /* Augmenter la taille du bouton */
   cursor: pointer;
-  font-size: 16px;
-  font-weight: 600;
+  font-size: 1.7rem; /* Augmenter la taille de la police */
+  color: white;
+  background: transparent;
+  letter-spacing: 10px; /* Espacement entre les lettres */
 `;
 
 const IntroductionPage = ({ onDiscover }) => {
   return (
     <IntroContainer>
       <Video autoPlay loop>
-        <source src="https://storage.googleapis.com/zayd-ecommerce.appspot.com/assets/Copie%20de%20Copie%20de%20LOGO%20VERSION%20FINALE.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
+        <source src="https://firebasestorage.googleapis.com/v0/b/zayd-ecommerce.appspot.com/o/assets%2FCopie%20de%20Copie%20de%20LOGO%20VERSION%20FINALE.mp4?alt=media&token=2465dabb-9daf-481b-b14c-0c0c44e7d43a" type="video/mp4" />
+        Votre navigateur ne prend pas en charge la balise vidéo.
       </Video>
       <DiscoverButton onClick={onDiscover}>Découvrir</DiscoverButton>
     </IntroContainer>
--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,46 @@
-import {createGlobalStyle} from "styled-components";
-import {CartContextProvider} from "@/components/CartContext";
-import {SessionProvider} from "next-auth/react";
-import WhatsappFloatButton from "@/components/WhatsappFloatButton";
+// pages/_app.js
+import { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { CartContextProvider } from '@/components/CartContext';
+import { SessionProvider } from 'next-auth/react';
+import WhatsappFloatButton from '@/components/WhatsappFloatButton';
+import IntroductionPage from '@/components/IntroductionPage';
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
-  body{
+  body {
     background-color: #eee;
-    padding:0;
-    margin:0;
+    padding: 0;
+    margin: 0;
     font-family: 'Poppins', sans-serif;
   }
-  hr{
+  hr {
     display: block;
-    border:0;
-    border-top:1px solid #ccc;
+    border: 0;
+    border-top: 1px solid #ccc;
   }
 `;
 
-export default function App({ Component, pageProps: {session, ...pageProps } }) {
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  const [showIntro, setShowIntro] = useState(true);
+
+  const handleDiscover = () => {
+    setShowIntro(false);
+  };
+
   return (
     <>
       <GlobalStyles />
-      <SessionProvider session={session}>
-        <CartContextProvider>
-          <Component {...pageProps} />
-          <WhatsappFloatButton />
-        </CartContextProvider>
-      </SessionProvider>
+      {showIntro ? (
+        <IntroductionPage onDiscover={handleDiscover} />
+      ) : (
+        <SessionProvider session={session}>
+          <CartContextProvider>
+            <Component {...pageProps} />
+            <WhatsappFloatButton />
+          </CartContextProvider>
+        </SessionProvider>
+      )}
     </>
   );
 }
