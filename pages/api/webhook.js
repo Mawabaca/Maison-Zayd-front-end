6f27012343a59377fdc5c22bab92d0109f065903
Abderahmane <abderahmane.benomari7@gmail.com>
2024-07-09 15:36:58 +0200

    ss
    ss

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,11 +1,11 @@
-import {mongooseConnect} from "@/lib/mongoose";
+import { mongooseConnect } from "@/lib/mongoose";
 const stripe = require('stripe')(process.env.STRIPE_SK);
-import {buffer} from 'micro';
-import {Order} from "@/models/Order";
+import { buffer } from 'micro';
+import { Order } from "@/models/Order";
 
 const endpointSecret = "whsec_fe743fd281fad511f4d99249c5fecd5a5467016526c6f40d1010924df0b7c380";
 
-export default async function handler(req,res) {
+export default async function handler(req, res) {
   await mongooseConnect();
   const sig = req.headers['stripe-signature'];
 
@@ -14,20 +14,37 @@ export default async function handler(req,res) {
   try {
     event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
   } catch (err) {
+    console.error(`Webhook Error: ${err.message}`);
     res.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
 
+  console.log(`Received event type ${event.type}`);
 
   switch (event.type) {
     case 'checkout.session.completed':
-      const data = event.data.object;
-      const orderId = data.metadata.orderId;
-      const paid = data.payment_status === 'paid';
-      if (orderId && paid) {
-        await Order.findByIdAndUpdate(orderId,{
-          paid:true,
-        })
+      const session = event.data.object;
+      const orderId = session.metadata.orderId;
+      const paymentIntent = session.payment_intent;
+
+      if (orderId && paymentIntent) {
+        try {
+          const paymentIntentData = await stripe.paymentIntents.retrieve(paymentIntent);
+          if (paymentIntentData.status === 'succeeded') {
+            const updatedOrder = await Order.findByIdAndUpdate(orderId, {
+              paid: true,
+            });
+            if (updatedOrder) {
+              console.log(`Order ${orderId} updated to paid.`);
+            } else {
+              console.error(`Order ${orderId} not found.`);
+            }
+          }
+        } catch (error) {
+          console.error(`Error updating order: ${error.message}`);
+        }
+      } else {
+        console.error(`Order ID or Payment Intent not found`);
       }
       break;
     default:
@@ -38,5 +55,5 @@ export default async function handler(req,res) {
 }
 
 export const config = {
-  api: {bodyParser:false,}
+  api: { bodyParser: false, }
 };
